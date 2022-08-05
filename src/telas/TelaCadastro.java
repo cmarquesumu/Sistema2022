d7ba3694ff3ee7bdb596238afea536bfab359cb8
cmarquesumu <86860386+cmarquesumu@users.noreply.github.com>
2022-08-05 07:32:18 -0300

    Aula 04/08/2022

--- src/telas/TelaCadastro.java
+++ src/telas/TelaCadastro.java
@@ -2,27 +2,41 @@ package telas;
 
 import java.awt.GridLayout;
 import javax.swing.JButton;
-import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
 
-public class TelaCadastro extends JFrame {
+public class TelaCadastro extends JInternalFrame {
     private JPanel painelBotoes =
             new JPanel();
-
+    private JButton jbIncluir = new JButton("Incluir");
+    private JButton jbAlterar = new JButton("Alterar");
+    private JButton jbExcluir = new JButton("Excluir");
+    private JButton jbConsultar = new JButton("Consultar");
+    private JButton jbConfirmar = new JButton("Confirmar");
+    private JButton jbCancelar = new JButton("Cancelar");
+    
     public TelaCadastro(String titulo) {
-        super(titulo);
+        super(titulo, true, true, true, true);
         setSize(500, 500);
-        getContentPane().add("North",
+        getContentPane().add("South",
                 painelBotoes);
         painelBotoes.setLayout(
                 new GridLayout(1, 6));
-        painelBotoes.add(new JButton("Incluir"));
-        painelBotoes.add(new JButton("Alterar"));
-        painelBotoes.add(new JButton("Excluir"));
-        painelBotoes.add(new JButton("Consultar"));
-        painelBotoes.add(new JButton("Confirmar"));
-        painelBotoes.add(new JButton("Cancelar"));
-        
-                
+        painelBotoes.add(jbIncluir);
+        painelBotoes.add(jbAlterar);
+        painelBotoes.add(jbExcluir);
+        painelBotoes.add(jbConsultar);
+        painelBotoes.add(jbConfirmar);
+        painelBotoes.add(jbCancelar);  
+        habilitaBotoes();
+    }
+    
+    public void habilitaBotoes() {
+        jbIncluir.setEnabled(true);
+        jbAlterar.setEnabled(false);
+        jbExcluir.setEnabled(false);
+        jbConsultar.setEnabled(true);
+        jbConfirmar.setEnabled(false);
+        jbCancelar.setEnabled(false);
     }
 }
--- src/telas/TelaSistema.java
+++ src/telas/TelaSistema.java
@@ -1,14 +1,16 @@
 package telas;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
 public class TelaSistema extends JFrame implements ActionListener {
-
+    private JDesktopPane jdp = new JDesktopPane();
     private JMenuBar jmb = new JMenuBar();
     private JMenu jmCadastros = new JMenu("Cadastros");
     private JMenu jmMovimentos = new JMenu("Movimentos");
@@ -21,6 +23,8 @@ public class TelaSistema extends JFrame implements ActionListener {
     public TelaSistema(String titulo) {
         setTitle(titulo);
         setSize(400, 300);
+        getContentPane().add(jdp);
+        jdp.setBackground(Color.yellow);
         setJMenuBar(jmb);
         jmb.add(jmCadastros);
         jmb.add(jmMovimentos);
@@ -29,6 +33,7 @@ public class TelaSistema extends JFrame implements ActionListener {
         adicionarItemMenu(jmCadastros, jmiEstado);
         adicionarItemMenu(jmCadastros, jmiCidade);
         adicionarItemMenu(jmCadastros, jmiFornecedor);
+        setExtendedState(MAXIMIZED_BOTH);
         setVisible(true);
         setDefaultCloseOperation(
                 JFrame.EXIT_ON_CLOSE);
@@ -44,15 +49,19 @@ public class TelaSistema extends JFrame implements ActionListener {
         if (ae.getSource() == jmiPais) {
             TelaCadastroPais telaCadastroPais = 
                     new TelaCadastroPais();
+            jdp.add(telaCadastroPais);
         } else if (ae.getSource() == jmiEstado) {
             TelaCadastroEstado telaCadastroEstado = 
                     new TelaCadastroEstado();
+            jdp.add(telaCadastroEstado);
         } else if (ae.getSource() == jmiCidade) {
             TelaCadastroCidade telaCadastroCidade = 
                     new TelaCadastroCidade();
+            jdp.add(telaCadastroCidade);
         } else if (ae.getSource() == jmiFornecedor) {
             TelaCadastroFornecedor telaCadastroFornecedor = 
                     new TelaCadastroFornecedor();
+            jdp.add(telaCadastroFornecedor);
         }
     }
 }
